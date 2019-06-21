6be02ab2d0696841e912e9513011be0383b1eb2b
a_bolt_of_lightning <46372775+a-bolt-of-lightning@users.noreply.github.com>
2019-06-21 19:32:36 +0430

    added input method

--- src/lotr/Main.java
+++ src/lotr/Main.java
@@ -1,45 +1,25 @@
 package lotr;
 
+import lotr.utility.ScannerWrapper;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Graph g1 = new Graph(4);
 
-        g1.addEdge(0,1);
-        g1.addEdge(0,2);
-        g1.addEdge(1,2);
-        g1.addEdge(1,3);
-        g1.addEdge(0,3);
+        int vertices = ScannerWrapper.getInstance().nextInt();
+        int edges = ScannerWrapper.getInstance().nextInt();
 
-        Graph g2 = new Graph(6);
+        Graph graph = new Graph(vertices);
 
-        g2.addEdge(0, 1);
-        g2.addEdge(2, 3);
-        g2.addEdge(2, 4);
-        g2.addEdge(3, 4);
+        for(int i=0; i<edges; i++){
+            int u = ScannerWrapper.getInstance().nextInt();
+            int v = ScannerWrapper.getInstance().nextInt();
+            graph.addEdge(u-1, v-1);
 
+        }
+        
+        System.out.println(graph.countCycle());
 
-        Graph g3 = new Graph(11);
-
-        g3.addEdge(0, 1);
-        g3.addEdge(0, 3);
-        g3.addEdge(1, 2);
-        g3.addEdge(3, 2);
-        g3.addEdge(3, 1);
-        g3.addEdge(2, 0);
-
-        g3.addEdge(5, 6);
-        g3.addEdge(4, 6);
-        g3.addEdge(4, 5);
-
-        g3.addEdge(7, 8);
-        g3.addEdge(10, 8);
-
-
-
-        System.out.println(g1.countCycle());
-        System.out.println(g2.countCycle());
-        System.out.println(g3.countCycle());
     }
 }
